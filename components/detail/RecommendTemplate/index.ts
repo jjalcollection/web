9084a341f430aabd85b78730d2ac0aff39ad96b3
leejiho9898 <cctv1998@naver.com>
2023-03-01 19:49:40 +0900

    feat: import 문 깔끔하게 변경

---
+++ components/detail/RecommendTemplate/index.ts
@@ -0,0 +1,1 @@
+export * from "./RecommendTemplate";
---
+++ containers/detail/RecommendJJalContainer.tsx
@@ -0,0 +1,10 @@
+import { RecommendTemplate } from "@/components/detail/RecommendTemplate";
+import React from "react";
+
+export function RecommendJJalContainer() {
+  return (
+    <>
+      <RecommendTemplate />
+    </>
+  );
+}
